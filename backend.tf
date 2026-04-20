29c10c560d081448659a3ed37372351369a65c47
royaditi686 <royaditi686@gmail.com>
2026-04-20 18:41:47 +0530

    Change DynamoDB table for Terraform backend

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "terraform-bucket-2026-project"
     key = "new31"
     region = "us-east-1"
-    dynamodb_table = "my-dynamodb-table"
+    dynamodb_table = "terraform-lock"
   }
 }
