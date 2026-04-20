bbd0c38c799ef96177f5f523d385910996680679
unknown <royaditi686@gmail.com>
2026-04-20 12:33:46 +0530

    1st commit

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-bucket-2026-project"
-    key = "main"
+    key = "new31"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
   }
--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "jenkins-us-east-11" 
+    default     =  "new31" 
 }
 
 variable "instance_type" { 
